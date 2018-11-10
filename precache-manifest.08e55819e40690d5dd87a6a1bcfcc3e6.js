23386f9ff865f09fb86ff1a7f4d750b554dd3336
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-11-10 16:02:41 +0100

    Updates

---
+++ precache-manifest.08e55819e40690d5dd87a6a1bcfcc3e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83d64611ff1ec6ecb19a",
+    "url": "/users-react/static/js/runtime~main.83d64611.js"
+  },
+  {
+    "revision": "87f321408cd07921c434",
+    "url": "/users-react/static/js/main.87f32140.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/users-react/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "87f321408cd07921c434",
+    "url": "/users-react/static/css/main.34c41bd2.chunk.css"
+  },
+  {
+    "revision": "68b3b47fbd3d1b9ad57cab1d52f1900b",
+    "url": "/users-react/index.html"
+  }
+];
